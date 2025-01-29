8c72d18720bc6b9875ea22aaef9f90aab16e5107
Ceydakesgin5 <ceydakesgin778@gmail.com>
2025-01-29 23:55:43 +0300

    mvc

---
+++ Basics/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+using Basics.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers{
+    public class CourseController:Controller
+    {
+        public IActionResult Index(){
+            return View();
+        }
+
+        public IActionResult Apply(){
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Apply(Candidate model){
+            return View();
+        }
+    }
+}
